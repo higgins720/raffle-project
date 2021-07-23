26f6ea9c13f287495120009ff860a5353336d83c
Diestroviche <jh072097@gmail.com>
2021-07-22 23:04:30 -0700

    Added text field, fixed issues

--- csv-interpreter.js
+++ csv-interpreter.js
@@ -1,56 +1,116 @@
-const myForm = document.getElementById("myForm");
+//Input Elements
+const uploadCSV = document.getElementById("uploadCSV");
 const csvFile = document.getElementById("csvFile");
-const winnerBox = document.getElementById("winnerBox");
+const nameInput = document.getElementById('addNames');
+const names = document.getElementById('names');
+
+//Output Elements
+const contestantBox = document.getElementById("contestantBox");
 const winnerList = document.getElementById("winnerList");
 const winnersInfo = document.getElementById('winnersInfo');
+const winnerBox = document.getElementById('winnerBox');
 
-var contestants = [];
-var winners = [];
+// Buttons
+const pickWinnerBtn = document.getElementById('pickWinner');
+pickWinnerBtn.disabled = true;
+const refreshBtn = document.getElementById('refresh');
+refreshBtn.disabled = true;
+const submitBtns = document.getElementsByClassName('submit-button');
+submitBtns.disabled = true;
 
-myForm.addEventListener("submit", function (e) {
+//Arrays
+const contestants = [];
+const winners = [];
 
-    e.preventDefault(); //Prevent page refresh on submission
+window.onload = function () {
+    console.log('Ready');
+}
 
-    const input = csvFile.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (event) {
-        var csvString = event.target.result; // the CSV content as string
-        //const chosen = text.findIndex(element => element == 2);
-        processData(csvString);
-        winnersInfo.innerHTML = (contestants);
-    };
-
-    reader.readAsText(input); //Read csv file
+//Add Contestants Form
+nameInput.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (names.value == '') {
+        alert('Nothing added');
+    } else {
+        processNameInput(names.value);
+        showContestants();
+        pickWinnerBtn.disabled = false;
+    }
 });
 
+function processNameInput(value) {
+    let vSplit = value.split(',');
+    for (let i = 0; i < vSplit.length; i++) {
+        contestants.push(vSplit[i]);
+    }
+}
+
+// Submit CSV
+uploadCSV.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (csvFile.value == '') {
+        alert('Nothing added');
+    } else {
+        let input = csvFile.files[0];
+        let reader = new FileReader();
+        reader.onload = function (event) {
+            var csvString = event.target.result; // CSV content as string
+            processData(csvString);
+            contestantBox.innerHTML = (contestants);
+            pickWinnerBtn.disabled = false;
+        }
+        reader.readAsText(input); //Read csv file
+    };
+});
+
+// Process Data
 function processData(csvString) {
     let allTextLines = csvString.split(/\r\n|\n/);
-    for (var i = 0; i < allTextLines.length; i++) {
-        var data = allTextLines[i].split(';');
+
+    for (let i = 0; i < allTextLines.length; i++) {
+        var row = allTextLines[i].split(';');
         var tarr = [];
-        for (var j = 0; j < data.length; j++) {
-            tarr.push(data[j]);
+        for (var j = 0; j < row.length; j++) {
+            tarr.push(row[j]);
         }
         contestants.push(tarr);
     }
 }
 
-document.getElementById('pickNext').addEventListener('click', function randomSelector(max) {
-    
-    // Pick random number to choose contestant
-    let select = Math.floor(Math.random() * contestants.length);
-    let winner = contestants[select];
+//Pick Winner Button
+pickWinner.addEventListener('click', function () {
 
-    // Post Winner
-    winnerBox.innerHTML = winner;
-    // Cut winner out of contestants array
-    let index = contestants.indexOf(select);
-    if (index > -1) {
-      array.splice(index, 1);
-    }
-    winnersInfo.innerHTML = (contestants);
-    // Paste winner into winners array
+    let number = Math.floor(Math.random() * contestants.length);
+
+    let winner = contestants[number];
+    let index = contestants.indexOf(winner);
+    contestants.splice(index, 1);
+    contestantBox.innerHTML = contestants;
+
     winners.push(winner);
-    winnerList.innerHTML = (winners);
+    winnerBox.innerHTML = winner;
+    winnerList.innerHTML = winners;
+
+    checkFinished();
 });
+
+//Refresh Button
+refreshBtn.addEventListener('click', function () {
+    location.reload();
+    return false;
+});
+
+function checkFinished() {
+    if (contestants.length === 0) {
+        pickWinnerBtn.disabled = true;
+        refreshBtn.disabled = false;
+        return;
+    }
+}
+
+function showContestants() {
+    if (contestants.length > 0) {
+        contestantBox.innerHTML = contestants;
+        return;
+    }
+}
